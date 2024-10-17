d6b80ba85f5c33612b2711284200b7e3d636d3f5
jeffT12 <jeff.tchakote@stu.ocsb.ca>
2024-10-17 14:44:08 +0000

    html talking to js

--- main.js
+++ main.js
@@ -9,7 +9,11 @@
 'use strict';
 
 /*** Event Listeners ***/
-
+document.getElementById("d6").addEventListener("click",roll_d6)
+document.getElementById("d8").addEventListener("click",roll_d8)
+document.getElementById("d6").addEventListener("mouseenter",d6_enter)
+document.getElementById("d6").addEventListener("mouseleave",d6_leave)
+document.getElementById("enter").addEventListener("click",greeting);
 
 /*** Functions ***/
 
@@ -18,3 +22,26 @@ function randInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+function roll_d6() {
+    let roll = randInt(1,6);
+    document.getElementById("die_roll").textContent = roll;
+}
+
+function roll_d8() {
+let roll = randInt(1,8);
+document.getElementById("die_roll").textContent = roll;
+}
+
+
+function d6_enter() {
+document.getElementById("die").textContent = "🎃"
+}
+
+function d6_leave() {
+    document.getElementById("die").textContent = "☠"
+}
+
+function greeting() {
+    let name = document.getElementById("user_input").value;
+    document.getElementById("greeting").textContent = `Hello ${name}!!`
+}
